6b44b2f63bc4e8331a78f5dc3352708502b0aa0a
missmahi55 <mahakagarwal248@gmail.com>
2022-04-17 18:25:53 +0530

    updations

--- src/components/events/EcEvents/Register/EcRegister.js
+++ src/components/events/EcEvents/Register/EcRegister.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Navbar from '../../../navbar/Navbar';
 import './EcRegister.css'
 import { Avatar } from "antd";
-import PidDisplay from '../../CsEvents/Register/PidDisplay';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import { Link } from 'react-router-dom';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    py:5,
+  };
 
 function EcRegister() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const [name, setName] = useState('')
+    const [rollno, setRollno] = useState('')
+    const [email, setEmail] = useState('')
+    const [mobileno, setMobileno] = useState('')
+    function onclick(e){
+        return(
+            name === "" ? "": 
+            rollno === "" ? "" : 
+            email === "" ? "" :
+            mobileno === "" ? "" : handleOpen()
+        )
+    }
+    function onsubmit(e){
+        e.preventDefault();
+        return false
+    }
     return (
         <div className="eceventregister">
             <Navbar/>
             <div className="regform">
-                <form className="form">
+                <form className="form" onSubmit={onsubmit}>
                 <Avatar className="avtr" src="https://joeschmoe.io/api/v1/random" />
                 <h3>Welcome</h3>
-                <input type="text" placeholder="Enter Name" />
-                <input type="number" placeholder="Enter Roll Number" />
-                <input type="email" placeholder="Enter Email" />
-                <input type="telnet" placeholder="Enter Mobile Number" />
+                <input type="text" placeholder="Enter Name" value={name} onInput={e => setName(e.target.value)} id="name" required={true}/>
+                <input type="number" name="rollnumber" placeholder="Enter Roll Number" value={rollno} onInput={e => setRollno(e.target.value)} required={true}/>
+                <input type="email" placeholder="Enter Email" value={email} onInput={e => setEmail(e.target.value)} required={true}/>
+                <input type="telnet" placeholder="Enter Mobile Number" value={mobileno} onInput={e => setMobileno(e.target.value)} required={true}/>
                 <label>Select Event:-</label>
                 <div className="eventselect">
                     <ul>
@@ -29,9 +63,27 @@ function EcRegister() {
                     </ul>
                 </div>
                 <br />
-                <button type="button" style={{marginBottom:'50px'}}>
-                   {<PidDisplay/>}
-                </button>
+                <button type="submit" style={{marginBottom:'50px'}} onClick={onclick}>Register</button>
+                <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                <div style={{textAlign:'center'}}>
+                    <h4 id="modal-modal-title" style={{color:'green'}}>
+                        You've Successfully registered!
+                    </h4>
+                    <br/>
+                    <h3>Your PID is:</h3>
+                    <h1>220145</h1>
+                    <Link to="/">
+                    <button onClick={handleClose} className="homebtn">Back to home</button>
+                    </Link>
+                </div>
+                </Box>
+            </Modal>
                 </form>
             </div>
         </div>
--- src/components/events/LiteraryClub/Register/LiteraryRegister.js
+++ src/components/events/LiteraryClub/Register/LiteraryRegister.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './LiteraryRegister.css';
 import { Avatar } from "antd";
 import Navbar from '../../../navbar/Navbar';
-import PidDisplay from '../../CsEvents/Register/PidDisplay';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import { Link } from 'react-router-dom';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    py:5,
+  };
+
 
 function LiteraryRegister() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const [name, setName] = useState('')
+    const [rollno, setRollno] = useState('')
+    const [email, setEmail] = useState('')
+    const [mobileno, setMobileno] = useState('')
+    function onclick(e){
+        return(
+            name === "" ? "": 
+            rollno === "" ? "" : 
+            email === "" ? "" :
+            mobileno === "" ? "" : handleOpen()
+        )
+    }
+    function onsubmit(e){
+        e.preventDefault();
+        return false
+    }
     return (
         <div className="literaryeventregister">
             <Navbar/>
             <div className="regform">
-                <form className="form">
+                <form className="form" onSubmit={onsubmit}>
                 <Avatar className="avtr" src="https://joeschmoe.io/api/v1/random" />
                 <h3>Welcome</h3>
-                <input type="text" placeholder="Enter Name" />
-                <input type="number" placeholder="Enter Roll Number" />
-                <input type="email" placeholder="Enter Email" />
-                <input type="telnet" placeholder="Enter Mobile Number" />
+                <input type="text" placeholder="Enter Name" value={name} onInput={e => setName(e.target.value)} id="name" required="required"/>
+                <input type="number" name="rollnumber" placeholder="Enter Roll Number" value={rollno} onInput={e => setRollno(e.target.value)} required={true}/>
+                <input type="email" placeholder="Enter Email" value={email} onInput={e => setEmail(e.target.value)} required={true}/>
+                <input type="telnet" placeholder="Enter Mobile Number" value={mobileno} onInput={e => setMobileno(e.target.value)} required={true}/>
                 <label>Select Event:-</label>
                 <div className="eventselect">
                     <ul>
@@ -29,9 +64,27 @@ function LiteraryRegister() {
                     </ul>
                 </div>
                 <br />
-                <button type="button" style={{marginBottom:'50px'}}>
-                   {<PidDisplay/>}
-                </button>
+                <button type="submit" style={{marginBottom:'50px'}} onClick={onclick}>Register</button>
+                <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                <div style={{textAlign:'center'}}>
+                    <h4 id="modal-modal-title" style={{color:'green'}}>
+                        You've Successfully registered!
+                    </h4>
+                    <br/>
+                    <h3>Your PID is:</h3>
+                    <h1>220145</h1>
+                    <Link to="/">
+                    <button onClick={handleClose} className="homebtn">Back to home</button>
+                    </Link>
+                </div>
+                </Box>
+            </Modal>
                 </form>
             </div>
         </div>
